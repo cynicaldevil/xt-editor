feea3bc9d819668bd0e2e1e7031b1bed4296e04d
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2017-03-19 00:38:16 +0530

    Shows line numbers

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -8,17 +8,56 @@ class TextEditor extends Component {
         };
     }
 
-    handleChange = (event) => {
-        this.setState({value: event.target.value});
+    handleChange = () => {
+        // ensure symmetry
+        let input_string = '<div>' + this.textArea.innerHTML;
+        console.log(input_string);
+        this.setState({
+            value: input_string,
+        });
     }
 
     render() {
+
+        const styles = {
+            main: {
+                display: 'flex',
+            },
+            linesDisplay: {
+                width: 30,
+                border: '1px solid black',
+                display: 'flex',
+                flexDirection: 'column',
+            },
+            lineNum: {
+                height: 18,
+                margin: 0,
+            },
+            textarea: {
+                width: 900,
+                height: 300,
+                border: '1px solid grey'
+            }
+        };
+
+        // TODO: Does not account for lines longer than width of editor. Make it so.
+        const num_lines_display = this.state.value.split(/<div>/).map((_, index) => {
+            return <p style={styles.lineNum} key={index}>{++index}<br /></p>;
+        });
+
+        // shave off the extra line number
+        num_lines_display.pop();
+
         return (
-            <div>
-                <textarea value={this.state.value}
-                          onChange={this.handleChange}
-                          ref={(input) => this.textArea = input }
-                          rows="20" cols="100" />
+            <div style={styles.main}>
+                <div style={styles.linesDisplay}>
+                    {num_lines_display}
+                </div>
+                <div contentEditable='true'
+                     value={this.state.value}
+                     onInput={this.handleChange}
+                     ref={(input) => this.textArea = input }
+                     style={styles.textarea} />
             </div>
         );
     }

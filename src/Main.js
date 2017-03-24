fb8cb0ef0b1e4ec5e23d96a147659b68af1a1e79
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2017-03-25 04:20:48 +0530

    Added warning and buttons

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import TextEditor from './TextEditor';
+import clipboard from '../public/clipboard-text.png';
+import help_circle from '../public/help-circle.png';
 
 class Main extends Component {
     constructor(props) {
@@ -38,6 +40,34 @@ class Main extends Component {
                 display: 'flex',
                 flexDirection: 'column',
                 marginRight: 1
+            },
+            controls: {
+                display: 'flex',
+                justifyContent: 'space-between',
+                width: '100%'
+            },
+            warning: {
+                visibility: this.props.error ? 'visible' : 'hidden',
+                fontSize: 12,
+                padding: 5,
+                height: 15,
+                color: '#FF1B1B',
+                backgroundColor: 'rgba(255, 127, 127, 0.2)',
+                border: '1px solid #FF1B1B'
+            },
+            clipboard: {
+                backgroundColor: '#222',
+                borderRadius: '50%',
+                border: '1px solid #f8f8f2',
+                cursor: 'pointer',
+                opacity: '.5',
+                textAlign: 'center',
+                transition: 'all .333s',
+                height: 40,
+                width: 40,
+                display: 'flex',
+                justifyContent: 'space-around',
+                marginLeft: 7,
             }
         };
 
@@ -65,10 +95,28 @@ class Main extends Component {
                 <div style={styles.sub}>
                     <h1 style={styles.heading}>xt-editor</h1>
                     <TextEditor />
-                    {this.props.error ? <p>Warning! Nested or unbalanced link tags!</p> : null}
-                    <Button type='button'
-                        onClick={this.handleClick}>Done</Button>
-                    <div style={styles.links}>{links}</div>
+                    <div style={styles.controls}>
+                        <p style={styles.warning}>
+                            Warning! Nested or unbalanced link tags!
+                        </p>
+                        <div>
+                            <div style={{display: 'flex',
+                                         alignItems: 'center',
+                                         paddingTop: 5}}>
+                            <button type='button' style={{
+                                                    ...styles.clipboard,
+                                                    border: '0px'}}>
+                                <img alt='help_circle' src={help_circle} />
+                            </button>
+                            <button type='button' style={styles.clipboard}>
+                                <img alt='clipboard' src={clipboard} />
+                            </button>
+                            <Button type='button'
+                                onClick={this.handleClick}>Done</Button>
+                            </div>
+                            <div style={styles.links}>{links}</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
